8bc6ad6020c0867f3d2b1aec8ae26d2d0d422451
TAEKGYUN-GWON <god_lion@naver.com>
2020-12-03 01:30:21 +0900

    에너미 수정중

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum E_ENEMY_TYPE : int
+{
+    NONE = 0,
+    BASIC = 1,
+    SPEED = 2
+}
+
 public class Enemy : MonoBehaviour
 {
-    public float speed = 10f;
+    private float speed = 0f;
 
+    private E_ENEMY_TYPE eType = E_ENEMY_TYPE.NONE;
     private Transform target;
     private int wayPointIdx = 0;
     private List<Transform> wayPoints;
+
+    public E_ENEMY_TYPE Type { get => eType; set => Type = CastTo<E_ENEMY_TYPE>.From(value); }
+
     void Start()
     {
         wayPoints = WayPonts.points;
